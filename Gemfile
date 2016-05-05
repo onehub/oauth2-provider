5f1816c6439298e9706c60db115488a9b570996c
Brian Moran <bmoran@onehub.com>
2016-05-05 13:37:19 -0700

    rails v4 and protected_attributes

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'https://rubygems.org'
 
+gem 'protected_attributes'
+
 group :development do
   gem 'activerecord', '>= 3.2.0' # The SQLite adapter in 3.1 is broken
 end
--- lib/songkick/oauth2/schema/20121025180447_songkick_oauth2_schema_add_unique_indexes.rb
+++ lib/songkick/oauth2/schema/20121025180447_songkick_oauth2_schema_add_unique_indexes.rb
@@ -3,11 +3,11 @@ class SongkickOauth2SchemaAddUniqueIndexes < ActiveRecord::Migration
 
   def self.up
     FIELDS.each do |field|
-      remove_index :oauth2_authorizations, [:client_id, field]
-      add_index :oauth2_authorizations, [:client_id, field], :unique => true
+      remove_index :oauth2_authorizations,  :name => "oa2_client_id_#{field.to_s}"
+      add_index :oauth2_authorizations, [:client_id, field], { :name => "oa2_client_id_#{field.to_s}", :unique => true }
     end
-    remove_index :oauth2_authorizations, [:access_token_hash]
-    add_index :oauth2_authorizations, [:access_token_hash], :unique => true
+    remove_index :oauth2_authorizations, :name =>  "oa2_access_token_hash"
+    add_index :oauth2_authorizations, [:access_token_hash], {:name => "oa2_access_token_hash", :unique => true}
 
     remove_index :oauth2_clients, [:client_id]
     add_index :oauth2_clients, [:client_id], :unique => true
@@ -17,11 +17,11 @@ class SongkickOauth2SchemaAddUniqueIndexes < ActiveRecord::Migration
 
   def self.down
     FIELDS.each do |field|
-      remove_index :oauth2_authorizations, [:client_id, field]
-      add_index :oauth2_authorizations, [:client_id, field]
+      remove_index :oauth2_authorizations,  :name => "oa2_client_id_#{field.to_s}"
+      add_index :oauth2_authorizations, [:client_id, field], :name => "oa2_client_id_#{field.to_s}"
     end
-    remove_index :oauth2_authorizations, [:access_token_hash]
-    add_index :oauth2_authorizations, [:access_token_hash]
+    remove_index :oauth2_authorizations,  :name => "oa2_access_token_hash"
+    add_index :oauth2_authorizations, [:access_token_hash], :name => "oa2_access_token_hash"
 
     remove_index :oauth2_clients, [:client_id]
     add_index :oauth2_clients, [:client_id]
